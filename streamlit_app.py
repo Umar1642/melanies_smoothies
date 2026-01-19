06b68ba80dd2ece9fd5a0f8bcc0a767a7e17c9b7
Umar Marikar <umar05marikar@gmail.com>
2026-01-19 12:41:21 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,8 +8,8 @@ from snowflake.snowpark.functions import col
 conn = st.connection("snowflake")
 session = conn.session()
 
-
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
+st.write(requests.get("https://api.ipify.org").text)
 st.write("Choose the fruits you want in your custom Smoothie!")
 
 name_on_order = st.text_input("Name on Smoothie:")
